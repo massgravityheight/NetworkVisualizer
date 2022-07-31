ba2abe5d8cb8c66caf12807f12e6ee38dac3886d
massgravityheight <106001192+massgravityheight@users.noreply.github.com>
2022-07-30 23:37:43 -0500

    This version successfully draws lines between nodes
    Right click to add line between previously selected node and selected node.
    
    Also removed bugs with extraneous menu nodes being created.

--- NetworkVisualizer22.07.30.py
+++ NetworkVisualizer22.07.30.py
@@ -12,6 +12,7 @@ WHITE = (255,255,255)
 GREY = (220, 220, 220)
 RED = (255,0,0)
 BLUE = (0,0,255)
+MENUBLUE = (0, 100, 255)
 GREEN = (0,255,0)
 Gravity = .5
 
@@ -26,6 +27,9 @@ NodeTextBufferY = NodeSizeY * 1.2
 NodeTextBufferX = 0
 NodeTextColor = GREY
 NodeTextSize = 20
+
+LineWidth = 4
+startp_x = startp_y = 0
 
 # --- classes ---
 class Sprite(pygame.sprite.Sprite):
@@ -59,6 +63,33 @@ class SpriteRect(pygame.sprite.Sprite):   # Used to create the Menu, probably di
         self.drag = False
         self.active = False
 
+class Line(pygame.sprite.Sprite):   # Used to create the lines between nodes
+    def __init__(self, color, firstX, firstY, secondX, secondY,screenH,screenW):
+        super().__init__()
+        
+        self.image = pygame.Surface([screenH, screenW])
+        self.image.fill(BLACK)
+        self.image.set_colorkey(BLACK)
+        
+#         if (secondX-firstX) < 0:
+#             actwidth = -1 * width
+#             actstartx = width
+#         else:
+#             actwidth = width
+#             actstartx = 0
+#         if (secondY-firstY) < 0:
+#             actheight = -1 * height
+#             actstarty = height
+#         else:
+#             actheight = height
+#             actstarty = 0
+        
+        pygame.draw.line(self.image, color, (firstX, firstY), (secondX, secondY), LineWidth)
+        self.rect = self.image.get_rect()
+        self.id = ""
+        self.drag = False
+        self.active = False
+        
 class Node(pygame.sprite.Sprite):
     def __init__(self, nodecolor, nodetextcolor, width, height, text, size):
         super().__init__()
@@ -80,6 +111,7 @@ class Node(pygame.sprite.Sprite):
         self.drag = False
         self.active = False
         self.atMenu = True
+        self.connections = []
         
     def update(self): # Called when .update is called for all sprites in sprite list below 
         if self.active: # Swap colors of the selected node for easier identification when typing
@@ -113,14 +145,31 @@ def load_objects(): # Opens a local csv file and extracts the data as a list. Co
     except Exception as ex:
         print("Error during loading csv file", ex)
         return False
-        
+
+def connect(prevSP, currSP, NodeGroup): # Looks up and grabs positions of previous Sprite & current Sprite by name text.
+    for sprite in NodeGroup: # Pull all Node sprite location and text
+        if prevSP == sprite.text:
+            startp_x = sprite.rect.x
+            startp_y = sprite.rect.y
+        if currSP == sprite.text:
+            finalp_x = sprite.rect.x
+            finalp_y = sprite.rect.y
+    
+    startp_x = startp_x + NodeTotalSizeX/2
+    startp_y = startp_y + NodeSizeY/2
+    finalp_x = finalp_x + NodeTotalSizeX/2
+    finalp_y = finalp_y + NodeSizeY/2
+    
+    print(startp_x, startp_y, finalp_x, finalp_y)
+    return startp_x, startp_y, finalp_x, finalp_y
+
 # --- main function --- Runs Once
 def main(): 
     
     # --Initiatilize The Pygame --
     pygame.init()
     clock = pygame.time.Clock()
-    
+
     # - Create The Screen & Local Variables -
     screenflags = pygame.FULLSCREEN
     screen = pygame.display.set_mode((0,0))#,screenflags) # Add screenflags back in to go fullscreen.
@@ -142,19 +191,28 @@ def main():
     me.id = "me"
     all_sprites_list.add(me) # Add me to the total list
     
+    # Create Menu
+    menu = SpriteRect(MENUBLUE, ((screenW / 6) - 25), (screenH - 100))
+    menu.rect.x, menu.rect.y = menuBorder
+    menu.id = "menu"
+    all_sprites_list.add(menu)
+    
     # Create Node
+     # Create all nodes from file
     NodeGroupInfo = load_objects()
-    if not NodeGroupInfo: # Create original layout of 1 node at menu if no file is found
-        NodeSprite = Node(GREEN, NodeTextColor, NodeTotalSizeY, NodeTotalSizeX, UserText, NodeTextSize)
+    if not NodeGroupInfo:
+        print("Nothing loaded.")
+        # Create original layout of 1 node at menu 
+        NodeSprite = Node(GREEN, NodeTextColor, NodeTotalSizeY, NodeTotalSizeX, UserText, NodeTextSize) 
         NodeSprite.rect.x, NodeSprite.rect.y = ((menuBorder[0] + 30), (menuBorder[1] + 30)) # Create Node that starts at menu
         NodeSprite.id = "node"
         all_sprites_list.add(NodeSprite)
-        NodeGroup.add(NodeSprite)        
+        NodeGroup.add(NodeSprite)
     else: # If there is a file uploaded successfully, load the position and text of all the nodes
         for i in range(0,len(NodeGroupInfo)): # Clumsy method to strip out the 4 values of the csv sprite rect
-            NodeRect = str(NodeGroupInfo[i][0])
-            print(NodeRect)
-            NodeRectStr = [0,0,0,0]
+            NodeRect = str(NodeGroupInfo[i][0]) # First column (0) in csv matrix is the rect string
+            #print(NodeRect) # Uncomment to have list of saved nodes printed to screen
+            NodeRectStr = [0,0,0,0] # Will be filled with the 4 last character positions of the 4 position values (x, y, width, height) of each sprite rect.
             fComma = 0
 
             for j in range(0,3):
@@ -162,22 +220,22 @@ def main():
                 NodeRectStr[j] = (str(fComma))
             fPara = NodeRect.find(')')
             NodeRectStr[3] = str(fPara)
-            NodeRect_x = NodeRect[6:int(NodeRectStr[0])]
-            NodeRect_y = NodeRect[(int(NodeRectStr[0])+1):int(NodeRectStr[1])]
-            NodeRect_w = NodeRect[(int(NodeRectStr[1])+1):int(NodeRectStr[2])]
-            NodeRect_h = NodeRect[(int(NodeRectStr[2])+1):fPara]
-            NodeFinalRect = pygame.Rect((int(NodeRect_x), int(NodeRect_y)), (int(NodeRect_w), int(NodeRect_h)))
-            NodeSprite = Node(GREEN, NodeTextColor, NodeTotalSizeY, NodeTotalSizeX, NodeGroupInfo[i][1], NodeTextSize)
+
+            NodeRect_x = NodeRect[6:int(NodeRectStr[0])] # Ripping 1st value between the established last character position.
+            NodeRect_y = NodeRect[(int(NodeRectStr[0])+1):int(NodeRectStr[1])] # Ripping 2nd value between the established last character position.
+            NodeRect_w = NodeRect[(int(NodeRectStr[1])+1):int(NodeRectStr[2])] # Ripping 3rd value between the established last character position.
+            NodeRect_h = NodeRect[(int(NodeRectStr[2])+1):fPara] # Ripping 4th value between the established last character position.
+            NodeFinalRect = pygame.Rect((int(NodeRect_x), int(NodeRect_y)), (int(NodeRect_w), int(NodeRect_h))) # Create final rect with ripped values.
+            
+            NodeSprite = Node(GREEN, NodeTextColor, NodeTotalSizeY, NodeTotalSizeX, NodeGroupInfo[i][1], NodeTextSize) # Second column (1) in csv matrix is the name string
             NodeSprite.rect = NodeFinalRect
             NodeSprite.id = "node"
+            if menu.rect.contains(NodeSprite.rect):
+                NodeSprite.atMenu = True
+            else:
+                NodeSprite.atMenu = False
             all_sprites_list.add(NodeSprite)
             NodeGroup.add(NodeSprite)
-    
-    # Create Menu
-    menu = SpriteRect((0, 100, 255), ((screenW / 6) - 25), (screenH - 100))
-    menu.rect.x, menu.rect.y = menuBorder
-    menu.id = "menu"
-    all_sprites_list.add(menu)
             
     # -- Main Loop --
     running = True
@@ -190,7 +248,7 @@ def main():
                 for sprite in NodeGroup: # On Quit, create a list to save the locations and text of each node
                     spriteinfo = [sprite.rect, sprite.text]
                     NodeGroupInfo.append(spriteinfo)
-                print(NodeGroupInfo)    
+                #print(NodeGroupInfo) # Uncomment to have list of saved nodes printed to screen 
                 save_objects(NodeGroupInfo) # Call a function to create a csv file and save the list to it.
                 running = False
             
@@ -199,46 +257,54 @@ def main():
                 for sprite in all_sprites_list: # Iterates over every sprite, checking for keyboard interaction
                     if sprite.active:
                         if event.key == pygame.K_BACKSPACE: # Checks for backspace seperately 
-                            sprite.text = sprite.text[:-1] # Clears text?
+                            sprite.text = sprite.text[:-1] # Clears text?      
                         else:
-                            sprite.text = sprite.text + event.unicode
-                            
-                        sprite.update()
+                            sprite.text = sprite.text + event.unicode   
+                        sprite.update() # .update() calls the sprite class' method defined as update()
+                        
             # - Mouse Events -        
-            
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
-                    for sprite in all_sprites_list: # Iterates over every sprite, checking for mouse interaction
-                        if sprite.rect.collidepoint(event.pos) and sprite.id == "node": # Tests if mouse pressed on friend sprite
+                    for sprite in NodeGroup: # Iterates over every sprite, checking for mouse interaction
+                        if sprite.rect.collidepoint(event.pos): # Tests if mouse pressed on node sprite
                             sprite.drag = True
                             sprite.active = True
-
                             mouse_x, mouse_y = event.pos
-                            offset_x = sprite.rect.x - mouse_x
+                            offset_x = sprite.rect.x - mouse_x # Gets an offset so dragging can be done relative to mouse pos
                             offset_y = sprite.rect.y - mouse_y
-                            
+                            previousSpriteName = sprite.text
+                            print(previousSpriteName)
                         else:
                             sprite.active = False
-
+                            
+                if event.button == 3:
+                    for sprite in NodeGroup:
+                        if sprite.rect.collidepoint(event.pos):
+                            sprite.connections.append(previousSpriteName) # Add previous sprite name to current sprite's connections attribute list.
+                            startp_x, startp_y, finalp_x, finalp_y = connect(previousSpriteName, sprite.text, NodeGroup) # Run function to determine positions of previous and current sprite. Should return 4 values - startp_x, startp_y, finalp_x, finalp_y                            
+                            LineSprite = Line(WHITE, startp_x, startp_y, finalp_x, finalp_y, screenH,screenW)
+                            LineSprite.rect = (0,0)
+                            all_sprites_list.add(LineSprite)
+                            
             elif event.type == pygame.MOUSEBUTTONUP:
                 if event.button == 1:
-                    for sprite in all_sprites_list: # Iterates over every sprite, checking for mouse interaction
+                    for sprite in all_sprites_list:
                         sprite.drag = False
-                
+
             elif event.type == pygame.MOUSEMOTION:
-                for sprite in all_sprites_list: # Iterates over every sprite, checking for mouse interaction
+                for sprite in NodeGroup: # Iterates over every sprite, checking for mouse interaction
                     if sprite.drag:
                         mouse_x, mouse_y = event.pos
                         sprite.rect.x = mouse_x + offset_x
                         sprite.rect.y = mouse_y + offset_y
                     
-                    if sprite.id == "node" and sprite.atMenu == True and not (menu.rect.contains(sprite.rect)): # If node leaves the menu, create another
+                    if  sprite.atMenu == True and not (menu.rect.contains(sprite.rect)): # If node leaves the menu, create another
                         NodeSprite = Node(GREEN, NodeTextColor, NodeTotalSizeY, NodeTotalSizeX, UserText, NodeTextSize)
                         NodeSprite.rect.x, NodeSprite.rect.y = ((menuBorder[0] + 30), (menuBorder[1] + 30))
                         NodeSprite.id = "node"
                         all_sprites_list.add(NodeSprite)
                         NodeGroup.add(NodeSprite)
-                    
+                        
                         sprite.atMenu = False
                             
         # - Updates (without draws) -
@@ -257,5 +323,3 @@ def main():
 
 # ---- Call The Function ----
 main()
-
-
